9d1996ac15f378bb1c31add61f1b82534f76fc59
kalganem <khaled.a.alganem@gmail.com>
2020-09-23 16:10:27 -0400

    gsea app panel

--- app.R
+++ app.R
@@ -1,62 +1,93 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyverse)
 library(fgsea)
 
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+    navbarPage("BioPathNet",
 
-    # Application title
-    titlePanel("BioPathNet"),
+    # Overview UI Tab ----
+    tabPanel("Overview"),
+    
+    # Input UI Tab ----
+    tabPanel("Input", 
+             
+             fileInput("inupt_file", label = "Upload your DGE file: ",
+                       accept = c(".csv")
+             ),
+             radioButtons("input_rank", "Rank By: ", choices = c("FC", "p.value")),
+             actionButton("ex_btn", "Example"),
+             dataTableOutput("preview_tbl"),
+             dataTableOutput("ranked_preview_tbl")
+             
+             ), 
+    
+    # GSEA UI Tab ----
+    tabPanel("GSEA", 
+             
+             tabsetPanel(#"output_panels",
+                 tabPanel("Options", 
+                          radioButtons("gsea_sp", "Species ", choices = c("Human", "Mouse", "Rat")),
+                          sliderInput("gsea_itr", label = "Permutation", min = 100,  max = 1000, value = 500, step=100),
+                          sliderInput("gsea_min", label = "Min", min = 5,  max = 50, value = 15, step=5),
+                          sliderInput("gsea_max", label = "Max", min = 100,  max = 1000, value = 500, step=100),
+                          
+                          actionButton("gsea_btn", label = "Run GSEA")
+                          ),
+                 # tabPanel("Rank Table", 
+                 #          dataTableOutput("ranked_preview_tbl")
+                 #          ),
+                 tabPanel("Table", 
+                          dataTableOutput("gsea_table")
+                          ),
+                 tabPanel("Top",
+                          fluidRow(
+                              shinydashboard::box(title = "Up", solidHeader = T,
+                              width = 8, collapsible = T,
+                              plotOutput("up_gsea")),
+                              shinydashboard::box(title = "Down", solidHeader = T,
+                              width = 8, collapsible = T,
+                              plotOutput("down_gsea"))
+                          
+                          )
+                          ),
+                 tabPanel("LeadingEdge",
+                          selectInput("gsea_path_sel", label = "Select Pathway: ", choices = list()),
+                          plotOutput("ind_gsea_plot")
+                          )
+             )
+             
+             ), 
+    
+    # Enrichr UI Tab ----
+    tabPanel("Enrichr",
+             tabsetPanel(#"output_panels",
+                 tabPanel("Options", 
+                          radioButtons("enrichr_list", "Select Genes: ", choices = c("Up", "Down", "Both"), selected = "Both"),
+                          #sliderInput("enrichr_num", label = "Permutation", min = 50,  max = 1000, value = 500, step=50),
+                          
+                          actionButton("enrichr_btn", label = "Run Enrichr")
+                 ),
+                 tabPanel("Table", 
+                          dataTableOutput("enrichr_tbl")
+                 )
+             )
+             ), 
+    
+    # iLINCS UI Tab ----
+    tabPanel("iLINCS"), 
+    
+    # Final Results UI Tab ----
+    tabPanel("Final Results")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            fileInput("inupt_file", label = "Upload your DEG file: ",
-                      accept = c(".csv")
-                      )
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tabsetPanel(#"output_panels",
-                        tabPanel("Preview", dataTableOutput("preview_tbl")),
-                        tabPanel("GSEA",
-                                 dataTableOutput("ranked_preview_tbl"),
-                                 actionButton("gsea_btn", label = "Run GSEA"),
-                                 dataTableOutput("gsea_table"),
-                                 fluidRow(
-                                     column(6,
-                                            plotOutput("up_gsea")
-                                     ),
-                                     column(6,
-                                            plotOutput("down_gsea")
-                                     )
-                                 ),
-                                 
-                                 selectInput("gsea_path_sel", label = "Select Pathway: ", choices = list()),
-                                 plotOutput("ind_gsea_plot")
-                                 
-                                 
-                                 )
-                        )
-        )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    gsea_sel <- reactiveValues()
+    input_file <- reactiveValues()
     
     # output$frame <- renderUI({
     #     test <- "https://maayanlab.cloud/clustergrammer/viz/5f5a506251ad21000e214918/small_38x29_clustergrammer_matrix.txt"
@@ -64,50 +95,66 @@ server <- function(input, output) {
     #     my_test
     # })
     
-    data_set0 <- reactive({
+    # Input Server ----
+    observe({
         req(input$inupt_file)
         inFile <- input$inupt_file
         x<-read.csv(inFile$datapath, header=T,stringsAsFactors = F)
-        x
-        
+        input_file$data <- x
     })
-
+    
+    # example input file
+    observeEvent(input$ex_btn, {
+        print("ex_btn")
+        input_file$data <- read.csv("files/15-1_BLA_ERvEE.csv", header=T,stringsAsFactors = F)
+    })
+    
     output$preview_tbl <- renderDataTable({
-        data_set0() %>% head()
+        input_file$data %>% head()
     })
     
     output$ranked_preview_tbl <- renderDataTable({
-        data_set0() %>% distinct(ID, .keep_all = T) %>% 
-            arrange(desc(FC)) %>% mutate(rank = row_number()) %>%
-            select(ID, FC, rank) -> ranked_tbl
-        
-        gsea_sel$table <- ranked_tbl %>% select(ID, FC)
-        
-        ranked_tbl
+        req(input_file$data)
+        input_file$data %>% distinct(ID, .keep_all = T) %>% 
+            {if (input$input_rank == "p.value") arrange(.,p) else arrange(.,desc(FC))} %>% 
+            mutate(rank = row_number()) %>%
+            select(ID, FC,p, rank) -> ranked_tbl
+        input_file$ranked <- ranked_tbl %>% select(ID, FC)
+        ranked_tbl %>% head()
     })
-    
-    
+
+    # GSEA Server ----
     observeEvent(input$gsea_btn, {
-        
-        gsea_pathway <- gmtPathways("./files/Rat_GO_AllPathways_with_GO_iea_April_01_2019_symbol.gmt")
-        
-        ranks <- setNames(gsea_sel$table$FC, gsea_sel$table$ID)
-        
-        print(head(ranks))
-        
-        print("done")
-        
-        fgseaRes <- fgsea(gsea_pathway, ranks, minSize=15, maxSize=500, nperm=1000)
-        
-        output$gsea_table <- renderDataTable({
-            fgseaRes
-
+        req(input_file$ranked)
+        print("run_gsea")
+        withProgress(message = "Running GSEA", value = 0, {
+            incProgress(0.3)
+            gsea_pathway <- gmtPathways("./files/gsea_genesets/c5.bp.v7.1.symbols.gmt")
+            ranks <- setNames(input_file$ranked$FC, input_file$ranked$ID)
+            #print(head(ranks))
+            incProgress(0.6)
+            fgseaRes <- fgsea(gsea_pathway, ranks, minSize=input$gsea_min, maxSize=input$gsea_max, nperm=input$gsea_itr)
+            incProgress(0.8)
+            #fgseaRes <- mutate(fgseaRes1, pathway = str_remove(pathway, "GO_"), pathway = str_replace_all(pathway, "_", " "))
+            
         })
-        # 
-        topPathwaysUp <- fgseaRes[ES > 0][head(order(pval), n=10), pathway]
-        topPathwaysDown <- fgseaRes[ES < 0][head(order(pval), n=10), pathway]
-        topPathways <- c(topPathwaysUp, rev(topPathwaysDown))
-
+        
+        # gsea table
+        output$gsea_table <- renderDataTable({
+            fgseaRes %>% select(-leadingEdge) %>% mutate_if(is.numeric, round, 3) %>% 
+                head(10)
+        })
+        
+        # pulling top pathways
+        topPathwaysUp <- fgseaRes %>% filter(ES > 0) %>% 
+            arrange(pval) %>% head(10) %>% 
+            pull(pathway)
+        
+        topPathwaysDown <- fgseaRes %>% filter(ES < 0) %>% 
+            arrange(pval) %>% head(10) %>% 
+            pull(pathway)
+        
+        # gsea ranked plot
         output$up_gsea <- renderPlot({
             plotGseaTable(gsea_pathway[topPathwaysUp], ranks, fgseaRes,
                           gseaParam=0.5)
@@ -118,21 +165,22 @@ server <- function(input, output) {
                           gseaParam=0.5)
         })
         
+        
+        # gsea leading edge
         updateSelectInput(session, "gsea_path_sel", choices = fgseaRes$pathway)
         
         output$ind_gsea_plot <- renderPlot({
             req(input$gsea_path_sel)
-            
             plotEnrichment(gsea_pathway[[input$gsea_path_sel]],
                            ranks)
-            
         })
-        
-        
-        
+ 
     })
     
-    
+    # Enrichr Server ----
+    # iLINCS Server ----
+    # Final Results Server ----
+      
 }
 
 # Run the application 
